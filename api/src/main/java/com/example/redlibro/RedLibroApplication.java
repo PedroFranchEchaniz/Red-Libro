52379192a16ab956f6d267b7a331a635b2fbbe01
Pedro Franch Echániz <pedrofe.av@gmail.com>
2024-02-19 20:32:43 +0100

    start api

---
+++ api/src/main/java/com/example/redlibro/RedLibroApplication.java
@@ -0,0 +1,13 @@
+package com.example.redlibro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedLibroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedLibroApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/example/redlibro/RedLibroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redlibro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedLibroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
