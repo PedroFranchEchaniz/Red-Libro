636d755aa1cd239bae333e0b49378acc6a9f2412
Pedro Franch Echániz <pedrofe.av@gmail.com>
2024-03-14 11:46:29 +0100

    gestión de errores

---
+++ api/src/main/java/com/example/redlibro/error/GlobalRestControllerAdvice.java
@@ -0,0 +1,46 @@
+package com.example.redlibro.error;
+
+import com.example.redlibro.error.model.impl.ApiErrorImpl;
+import com.example.redlibro.error.model.impl.ApiValidationSubError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+@RestControllerAdvice
+
+public class GlobalRestControllerAdvice {
+    private final ResponseEntity<Object> buildApiError(String message, WebRequest request, HttpStatus status) {
+        return ResponseEntity
+                .status(status)
+                .body(
+                        ApiErrorImpl.builder()
+                                .status(status)
+                                .message(message)
+                                .path(((ServletWebRequest) request).getRequest().getRequestURI())
+                                .build()
+                );
+    }
+
+    private final ResponseEntity<Object> buildApiErrorWithSubErrors(String message, WebRequest request, HttpStatus status, List<ObjectError> subErrors) {
+        return ResponseEntity
+                .status(status)
+                .body(
+                        ApiErrorImpl.builder()
+                                .status(status)
+                                .message(message)
+                                .path(((ServletWebRequest) request).getRequest().getRequestURI())
+                                .subErrors(subErrors.stream()
+                                        .map(ApiValidationSubError::fromObjectError)
+                                        .collect(Collectors.toList())
+                                )
+                                .build()
+                );
+
+    }
+}
