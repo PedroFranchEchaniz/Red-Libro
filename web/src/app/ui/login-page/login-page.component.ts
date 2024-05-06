234912510a371e52fa9173611f43b49d7e1ce58d
Pedro Franch Echániz <pedrofe.av@gmail.com>
2024-05-06 19:19:47 +0200

    error en el login

--- web/src/app/ui/login-page/login-page.component.ts
+++ web/src/app/ui/login-page/login-page.component.ts
@@ -1,34 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountServiceService } from '../../service/AccountService/account-service.service';
 import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
-
   username = '';
   password = '';
-  rol: string[] = [];
-  id = '';
+  errorMessage = '';  // Variable para almacenar el mensaje de error
 
-  constructor(private accountService: AccountServiceService, private router: Router,) { }
+  constructor(private accountService: AccountServiceService, private router: Router) { }
 
   ngOnInit(): void {
     localStorage.clear();
 
     let token = localStorage.getItem('token');
-
-    if (token != null) {
-      this.router.navigateByUrl('/home');
+    if (token) {
+      this.router.navigateByUrl('/listBook');
     }
   }
 
   login() {
-    this.accountService.loginAccount(this.username, this.password).subscribe(() => {
-      this.router.navigateByUrl('/listBook');
-    })
+    this.accountService.loginAccount(this.username, this.password).subscribe({
+      next: (response) => {
+        this.router.navigateByUrl('/listBook');
+      },
+      error: (error: HttpErrorResponse) => {
+        // Aquí manejas el error
+        if (error.status === 401) {
+          this.errorMessage = 'Username or Password is incorrect';
+        } else {
+          this.errorMessage = 'An error occurred. Please try again later.';
+        }
+      }
+    });
   }
 }
